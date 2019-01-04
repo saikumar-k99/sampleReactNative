eb0c215dcb9675c17989e42149782808ccdf42b4
SAIKUMAR KANKIPATI <kankipatisaikumar@gmail.com>
2019-01-04 15:48:34 -0600

    Added Header component

--- DoorDashReactDemo/index.js
+++ DoorDashReactDemo/index.js
@@ -1,9 +1,13 @@
 // Import a library to help create a component(like a view or content)
+// use this kind of import for packages installed through npm
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
+// import any components that we create to reuse
+import Header from './src/Components/Header';
 
 // Create a component
-const App = () => (<Text>Hello World</Text>);
+// returns the JSX in the below funciton
+const App = () => (<Header />);
 
 // Render it to the device
 AppRegistry.registerComponent('DoorDashReactDemo', () => App);
---
+++ DoorDashReactDemo/src/Components/Header.js
@@ -0,0 +1,11 @@
+// info: Header is a single lable view 
+
+// import needed libraries 
+import React from 'react';
+import { Text } from 'react-native';
+
+// create the component
+const Header = () => (<Text>Restaurants</Text>);
+
+// make it available for rest of the app to use it to render on the screen
+export default Header;
