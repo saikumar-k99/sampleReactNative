0022de15a5f66e31cac13cc203f6ace2acc0a2b2
SAIKUMAR KANKIPATI <kankipatisaikumar@gmail.com>
2019-01-07 14:06:32 -0600

    added styling for header component

--- DoorDashReactDemo/src/Components/Header.js
+++ DoorDashReactDemo/src/Components/Header.js
@@ -5,7 +5,18 @@ import React from 'react';
 import { Text } from 'react-native';
 
 // create the component
-const Header = () => (<Text>Restaurants</Text>);
+const Header = () => {
+    const { textStyle } = styles;
+
+    return <Text style={textStyle}>Restaurants</Text>;
+ };
+
+const styles = {
+    textStyle: {
+        //Styles use camel case instead of - notation as in CSS
+        fontSize: 80
+    }
+};
 
 // make it available for rest of the app to use it to render on the screen
 export default Header;
