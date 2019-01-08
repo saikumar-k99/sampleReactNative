2052921ef6ac428e966d55d6ec0b060a9fc79653
SAIKUMAR KANKIPATI <kankipatisaikumar@gmail.com>
2019-01-08 10:30:55 -0600

    added a place holder for albums list

--- DoorDashReactDemo/index.js
+++ DoorDashReactDemo/index.js
@@ -1,13 +1,17 @@
 // Import a library to help create a component(like a view or content)
 // use this kind of import for packages installed through npm
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 // import any components that we create to reuse
 import Header from './src/Components/Header';
+import AlbumsList from './src/Components/AlbumList';
 
 // Create a component
 // returns the JSX in the below funciton
-const App = () => (<Header headerText={'Restaurants'}/>);
+const App = () => (<View>
+    <Header headerText={'Restaurants'} />
+    <AlbumsList />
+</View>);
 
 // Render it to the device
 AppRegistry.registerComponent('DoorDashReactDemo', () => App);
---
+++ DoorDashReactDemo/src/Components/AlbumList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumsList = () => (<View> 
+        <Text>Album list!! </Text>
+        </View>);
+
+export default AlbumsList;
