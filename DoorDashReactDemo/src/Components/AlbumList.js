388dc616a41e9b2af85885742d2c8f5894e44680
SAIKUMAR KANKIPATI <kankipatisaikumar@gmail.com>
2019-01-08 13:33:59 -0600

    added first life cycle method

--- DoorDashReactDemo/src/Components/AlbumList.js
+++ DoorDashReactDemo/src/Components/AlbumList.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumsList extends Component {
+
+    componentWilMount() {
+        console.log('componentWilMount is called now');
+    }
+
     render() {
-        return (<View> 
+        return (<View>
             <Text>Album list!! </Text>
             </View>);
     }
